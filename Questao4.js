66183b5bc80b3822520dd5aee1ba82cd6450c301
Nicole Pessoa <165043516+nicolepess@users.noreply.github.com>
2026-03-03 11:48:20 -0300

    Fix typo in variable declaration and add loop

--- Questao4.js
+++ Questao4.js
@@ -1,1 +1,5 @@
-let num = Nunmber(prompt("Escolha um número de 1 á 10: "))
+let num = Number(prompt("Escolha um número de 1 á 10: "));
+
+for(let i = 1; i <=10; i++){
+console.log(num + "x" + i + "=" + (num *i))
+}
