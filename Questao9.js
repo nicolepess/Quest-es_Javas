34ff99e94bf4c663071f9555f301a8c24aaacebb
Nicole Pessoa <165043516+nicolepess@users.noreply.github.com>
2026-03-05 12:18:22 -0300

    Create Questao9.js

---
+++ Questao9.js
@@ -0,0 +1,10 @@
+console.log("--AREA DE LOGIN--");
+
+let user= admin;
+let senha= 1234;
+let contadorU= 0;
+let contadorS= 0;
+
+while (contadorU < 3){
+  console.log(prompt("Digite seu nome de usuário: " user));
+  
