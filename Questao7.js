590d7eff0e5251c0d7c9f4c77ff7d419a296ac7c
Nicole Pessoa <165043516+nicolepess@users.noreply.github.com>
2026-03-05 11:46:00 -0300

    Soma até digitar zero

--- Questao7.js
+++ Questao7.js
@@ -1,1 +1,14 @@
+let soma = 0;
+let numero;
 
+while (true) {
+  numero = Number(prompt("Digite um número (0 para parar):"));
+
+  if (numero === 0) {
+    break;
+  }
+
+soma = soma + numero;
+}
+
+alert("A soma dos números digitados é: " + soma);
