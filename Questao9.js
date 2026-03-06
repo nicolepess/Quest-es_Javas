aa641432e88899f678f4f389f6a0fa3b1c33cb9f
Nicole Pessoa <165043516+nicolepess@users.noreply.github.com>
2026-03-05 22:39:10 -0300

    Update Questao9.js

--- Questao9.js
+++ Questao9.js
@@ -1,10 +1,31 @@
 console.log("--AREA DE LOGIN--");
 
-let user= admin;
-let senha= 1234;
-let contadorU= 0;
+let user= "admin";
+let senha= "1234";
 let contadorS= 0;
+let contadorU= 0;
 
-while (contadorU < 3){
-  console.log(prompt("Digite seu nome de usuário: " user));
-  
+while (contadorU< 3){
+  let usuarioDigitando = prompt("Digite seu nome de usuário: ");
+   
+   if(usuarioDigitando === user){
+       console.log("Usuário correto!");
+       break;
+   }else{
+           console.log("Usuario incorreto")
+        
+       }
+       contadorU++;
+} 
+ while (contadorS < 3){
+    let tentativaSenha =  prompt("Digite sua senha: ");
+    
+    if(tentativaSenha === senha){
+    console.log("Acesso liberado!");
+       break;
+       }else{
+           console.log("Tente novamente ");
+       }
+       contadorS++
+       
+}
