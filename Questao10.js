c99ccaf2e1ae5e4e01bfa1573a6ef9a989675032
Nicole Pessoa <165043516+nicolepess@users.noreply.github.com>
2026-03-17 09:24:04 -0300

    fatorial

--- Questao10.js
+++ Questao10.js
@@ -1,3 +1,14 @@
-let num = parseInt(prompt("Digite um número inteiro positivo: ");
+let num = parseInt(prompt("Digite um número inteiro positivo: "));
+let result = 1;
 
-const calculo = num
+if( num < 0 ){
+  console.log("Não existe fatorial de número negativo");
+}else{
+for(let i = 1; i <= num; i++){
+  result *= i;
+
+}
+  console.log("A fatorial do número " + num + " é igual : " + result);
+ 
+}
+  
